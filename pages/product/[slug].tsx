5f13342f5538f7b4b2d92e1e3f0b4a7163eb3c68
Thomas <thomasmathew365@gmail.com>
2022-02-11 11:04:40 +0900

    added product

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -1,10 +1,16 @@
 import { useRouter } from 'next/router';
+import Product from '../../components/product/product';
 
 const Post = () => {
 	const router = useRouter();
 	const { slug } = router.query;
 
-	return <p>Product: {slug}</p>;
+	return (
+		<div>
+			<p>Product: {slug}</p>
+			<Product />
+		</div>
+	);
 };
 
 export default Post;
